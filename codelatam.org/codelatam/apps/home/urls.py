c953e80d17de377ab34e051b3669837bc200133e
Alex Dzul <alexexc2@gmail.com>
2012-07-10 18:25:15 -0500

    contacto y about añadido

--- codelatam.org/codelatam/apps/home/urls.py
+++ codelatam.org/codelatam/apps/home/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls.defaults import patterns,url
 
 urlpatterns = patterns('codelatam.apps.home.views',
 	url(r'^$','index_view',name="vista_principal"),
+	url(r'^about/$','about_view',name="vista_about"),
+	url(r'^contacto/$','contact_view',name="vista_contacto"),
 )
