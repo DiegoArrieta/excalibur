ada4f73dc938bd52af4530cbb4e388473602df25
Alex Dzul <alexexc2@gmail.com>
2012-07-11 18:29:20 -0500

    HTML proximamente añadido
    Se agrega una nueva estructura de las URL's

--- codelatam.org/codelatam/apps/home/urls.py
+++ codelatam.org/codelatam/apps/home/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import patterns,url
 
 urlpatterns = patterns('codelatam.apps.home.views',
-	url(r'^$','index_view',name="vista_principal"),
-	url(r'^about/$','about_view',name="vista_about"),
-	url(r'^contacto/$','contact_view',name="vista_contacto"),
+	url(r'^$','proximamente_view',name="vista_proximamente"),
+	url(r'^beta/$','index_view',name="vista_principal"),
+	url(r'^beta/about/$','about_view',name="vista_about"),
+	url(r'^beta/contacto/$','contact_view',name="vista_contacto"),
 )
--- codelatam.org/codelatam/apps/home/views.py
+++ codelatam.org/codelatam/apps/home/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 
+def proximamente_view(request):
+	return render_to_response('proximamente.html',context_instance=RequestContext(request))
+
 def index_view(request):
 	return render_to_response('home/index.html',context_instance=RequestContext(request))
 	
