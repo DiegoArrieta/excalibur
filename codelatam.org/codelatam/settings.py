143c7d6c37ac109b068956bc0730df7c7b863776
Alex Dzul <alexexc@hotmail.com>
2012-07-11 23:07:19 -0500

    cambios en settings.py
    cambios en las configuraciones

--- codelatam.org/codelatam/settings.py
+++ codelatam.org/codelatam/settings.py
@@ -5,7 +5,6 @@ Modificado: Alex Dzul 10/07/2012
 Motivo: Para el dinamismo entre sistemas operativos
 """
 import os
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 # Esta linea de codigo se cambia cuando se trabaja localmente
@@ -40,7 +39,6 @@ else: # De lo contrario si es local entonces se utiliza la siguiente configuraci
 				'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
 			}
 		}
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -113,8 +111,4 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'codelatam.apps.home',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
 )
