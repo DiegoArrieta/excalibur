1967c30f613d8793361af4f05b096b1a0aa1904e
Alex Dzul <alexexc2@gmail.com>
2012-07-13 15:03:58 -0500

    udpate url
    url agregada

--- codelatam.org/codelatam/apps/home/urls.py
+++ codelatam.org/codelatam/apps/home/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import patterns,url
 
 urlpatterns = patterns('codelatam.apps.home.views',
 	url(r'^beta/$','index_view',name="vista_principal"),
+	url(r'^$','proximamente_view',name="vista_proximamente"),
 )
